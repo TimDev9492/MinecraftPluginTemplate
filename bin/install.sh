dd6e411ad59b06aad3fd29ee403521a44296b16b
Tim Kluge <tim@timdev.de>
2026-02-09 22:47:17 +0100

    feat: add exception package

--- bin/install.sh
+++ bin/install.sh
@@ -85,6 +85,7 @@ find "${PACKAGE_DIR}" -type f | while read -r file; do
   sed -i "s/^package ${INITIAL_GROUP_ID}.${INITIAL_PACKAGE_NAME}/package ${GROUP_ID}.${PACKAGE_NAME}/" "${file}"
 done
 mv "${PACKAGE_DIR}/${INITIAL_PROJECT_NAME}.java" "${PLUGIN_JAVA_FILE}"
+mv "${PACKAGE_DIR}/exceptions/${INITIAL_PROJECT_NAME}Exception.java" "${PACKAGE_DIR}/exceptions/${PLUGIN_NAME}Exception.java"
 
 sed -i "s/^name: ${INITIAL_PROJECT_NAME}/name: ${PLUGIN_NAME}/" src/main/resources/plugin.yml
 sed -i "s/^main: ${INITIAL_GROUP_ID}.${INITIAL_PACKAGE_NAME}.${INITIAL_PROJECT_NAME}/main: ${GROUP_ID}.${PACKAGE_NAME}.${PLUGIN_NAME}/" src/main/resources/plugin.yml
---
+++ src/main/java/me/author/minecraftPluginTemplate/exceptions/MinecraftPluginTemplateException.java
@@ -0,0 +1,7 @@
+package me.author.minecraftPluginTemplate.exceptions;
+
+public class MinecraftPluginTemplateException extends RuntimeException {
+    public MinecraftPluginTemplateException(String message) {
+        super(message);
+    }
+}
